4169bcb3917177e379e3489e31f4e3ef9ccf03fa
Manuel <manuelsanti.dev1996@gmail.com>
2023-07-05 13:35:07 +0200

    ciao sono gianni

---
+++ Home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+
+st.title("hello gianni!")
+st.header("Gianni")
+df = pd.read_csv("https://storage.googleapis.com/cloud-boot-app-bucket/1553768847-housing.csv")
+
+st.dataframe(df)
